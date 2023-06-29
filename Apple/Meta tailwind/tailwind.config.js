f7384802054f715c6e54ac798c448e110172fd5e
AristideI <a.isingizwe@alustudent.com>
2023-06-29 19:25:29 +0200

    Mutware

--- Apple/Meta tailwind/tailwind.config.js
+++ Apple/Meta tailwind/tailwind.config.js
@@ -2,11 +2,29 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
+
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '800px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '450px'},
+      // => @media (max-width: 639px) { ... }
+    },
+
     extend: {
       colors: {
         mainBlue: "#1A4072",
         orangemain: "#FDC73D"
-      }
+      },
     },
   },
   plugins: [],
